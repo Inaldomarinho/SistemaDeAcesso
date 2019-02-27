f1a0ba07ac6cf87d6b63e08b27084f22f0a904ac
inaldomarinho <inaldomarinhoqueirozneto@gmail.com>
2019-02-26 21:17:33 -0300

    26 de fev de 2019

--- Sistema Porta/core/API/viewsets.py
+++ Sistema Porta/core/API/viewsets.py
@@ -8,19 +8,26 @@ class PessoaViewSet(ModelViewSet):
     """
     A simple ViewSet for viewing and editing accounts.
     """
-    queryset = pessoa.objects.filter(ativado = True).order_by('nome')
+    queryset = pessoa.objects.filter(ativado=True).order_by('nome')
     serializer_class = PessoaSerializer
-    http_method_names = ['get', 'post']
+    http_method_names = ['get',]
 
-    def create(self, request, *args, **kwargs):
-       try:
-        aux = pessoa()
-        aux.nome = "Cadastrado pela tranca"
-        aux.key = request.data['key']
-        aux.save()
-        return  Response({'status' : 'sucesso'})
-       except:
-        return Response({'status': 'falha'})
+    def list(self, request, *args, **kwargs):
+
+       key = request.GET.get('key' or None)
+       if key != None:
+           try:
+            aux = pessoa()
+            aux.nome = "Cadastrado pela tranca"
+            aux.key = key
+            aux.save()
+            return  Response({'status' : 'sucesso'})
+           except:
+            return Response({'status': 'falha'})
+       else:
+           pessoas = pessoa.objects.filter(ativado=True).order_by('nome')
+           data = PessoaSerializer(pessoas, many=True).data
+           return Response(data)
 
 
 
@@ -28,13 +35,19 @@ class LogsViewSet(ModelViewSet):
     
     queryset = log.objects.all()
     serializer_class = LogSerializer
-    http_method_names = ['post']
+    http_method_names = ['get']
 
-    def create(self, request, *args, **kwargs):
-       try:
-        aux = log()
-        aux.key = get_object_or_404(pessoa, key=request.data['key'])
-        aux.save()
-        return  Response({'status' : 'sucesso'})
-       except:
-        return Response({'status': 'falha'})
+    def list(self, request, *args, **kwargs):
+
+        key = request.GET.get('key' or None)
+
+        if key != None:
+           try:
+            aux = log()
+            aux.key = get_object_or_404(pessoa, key=key)
+            aux.save()
+            return Response({'status': 'sucesso'})
+           except:
+            return Response({'status': 'falha'})
+        else:
+            return Response({'key': 'Insira uma key válida nesse campo'})
--- Sistema Porta/portaautomatica/settings.py
+++ Sistema Porta/portaautomatica/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(ty62@4*651#(zbba_=uq77l**4p*i0e$l9112-is)nl)n_ctj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.25.222", "127.0.0.1"]
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
